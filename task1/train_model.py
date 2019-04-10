6ac26bd45ea4246c1087f1bc7bf476db35403aa2
Ryder McMinn <mcminnra@149-161-178-196.dhcp-bl.indiana.edu>
2019-04-10 11:02:57 -0400

    switch dataset division order

--- task1/train_model.py
+++ task1/train_model.py
@@ -65,10 +65,10 @@ print(f"Loading data...DONE! [{end_time} seconds]")
 # Train Validation Split
 print("Creating Train/Validation/Test Set...", end="\r")
 start_time = time.time()
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=42)
 X_train, X_test, y_train, y_test = train_test_split(
-    X_train, y_train, test_size=0.20, random_state=42
+    X, y, test_size=0.20, random_state=42
 )
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.20, random_state=42)
 end_time = np.round(time.time() - start_time, 2)
 print(f"Creating Train/Validation/Test Set...DONE! [{end_time} seconds]")
 
