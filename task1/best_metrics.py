1e277d58bc25daa854680a71987211ee9b3f65e0
Ryder McMinn <mcminnra@ryders-mbp.attlocal.net>
2019-04-30 16:44:25 -0400

    updated metrics

--- task1/best_metrics.py
+++ task1/best_metrics.py
@@ -6,6 +6,7 @@ import time
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import load_model
+from sklearn.metrics import confusion_matrix
 
 # MacOS Fix
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
@@ -25,10 +26,73 @@ print(f"Loading data...DONE! [{end_time} seconds]")
 # Load Best Weights
 model = load_model("weights/model.h5")
 
-# Results
-print()
+# Make prediction on samples
+y_preds = model.predict(X_test)
+
+# Round percents to binary labeling
+for i, row in enumerate(y_preds):
+    for j, category in enumerate(row):
+        y_preds[i][j] = np.round(category, 0)
+
+num_categories = len(y_preds[0])
+num_samples = len(y_preds)
+print(f'Number of Categories: {num_categories}')
+print(f'Number of Samples: {num_samples}')
+
+# Precision & Recall
+# Pulled from https://stackoverflow.com/questions/9004172/precision-recall-for-multiclass-multilabel-classification
+precisions = []
+recalls = []
+mccs = []
+for i in range(0, num_categories):
+    class_preds = y_preds[:, i]
+    class_test = y_test[:, i]
+    cm = confusion_matrix(class_test, class_preds)
+
+    if len(cm) != 1:
+        tp = cm[0][0]
+        fp = cm[0][1]
+        tn = cm[1][1]
+        fn = cm[1][0]
+
+        # Precision
+        if tp == 0 and fp == 0:
+            precision = 0
+        else:
+            precision = tp / (tp + fp)
+        precisions.append(precision)
+
+        # Recall
+        if tp == 0 and fn == 0:
+            recall = 0
+        else:
+            recall = tp / (tp + fn)
+        recalls.append(recall)
+
+        # Matthew's Correlation Coefficient
+        if ((tp*tn) - (fp*fn)) != 0 or np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)) != 0:
+            mcc = ((tp*tn) - (fp*fn)) / np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+        else:
+            mcc = 0
+        mccs.append(mcc)
+    else:
+        precisions.append(1)
+        recalls.append(1)
+        mccs.append(1)
+    
+avg_precision = np.mean(precisions)
+avg_recall = np.mean(recalls)
+avg_mcc = np.mean(mccs)
+
+# Loss & Accuracy
 loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
-print("Testing - Acc: {:.4f}, Loss: {:.4f}".format(accuracy, loss))
+
+# Print Metrics
+print(f'Loss: {loss}')
+print(f'Accuracy: {accuracy}')
+print(f'Average Precisions across all classes: {avg_precision}')
+print(f'Average Recalls across all classes: {avg_recall}')
+print(f'Average MCC across all classes: {avg_mcc}')
 
 end_time = np.round(time.time() - file_time, 2)
 print(f"Total Training Time: {end_time} seconds")
